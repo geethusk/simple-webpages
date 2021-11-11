b982b883a613dfda3f4d07edf59dc4584516b1a0
geethu <geethukallada1@gmail.com>
2021-11-11 16:20:13 +0530

    arithematic

--- class3/script.js
+++ class3/script.js
@@ -1,14 +1,15 @@
 let result=0;
 let operatorString="";
-
-let operatorsArray=[];
-let operandsArray=[];
+let i=0;
+let operator;
+let operatorsArray=[];   //to define operators
+let operandsArray=[];   //to define opearnds
 let isLastOperator=true;
 
-const inputFields=document.getElementsByClassName("input_button");
+ const inputFields=document.getElementsByClassName("input_button");  //to get all elements in the array for input elements that is 16 elements including numbers and operators
 
 for(let i=0;i<inputFields.length;i++){
-    inputFields[i].addEventListener("click",listener);
+    inputFields[i].addEventListener("click",listener);  //listener is a function
 }
 
 
@@ -23,9 +24,12 @@ if(!event.target.getAttribute("role" )){
     isLastOperator= false;
     operatorString+=event.target.innerText;
 }
+
 else
 if(event.target.getAttribute("role")=== "submit" ){
-    console.log("submit");
+    if(!isLastOperator){
+        findResult();
+    }
 }else
 {
     if(!isLastOperator){
@@ -35,7 +39,7 @@ if(event.target.getAttribute("role")=== "submit" ){
     isLastOperator=true;
 }
 
-console.log(operandsArray,operatorsArray);
+//console.log(operandsArray,operatorsArray);
 
 if(!operatorString){
     document.getElementById("operators_section").innerText=0;
@@ -48,4 +52,35 @@ document.getElementById("result_section").innerText=result;
 
 }
 
+function findResult(){
+  console.log(operandsArray,operatorsArray);
+  console.log(Number(operandsArray[0]));
+    while(operandsArray.length!=0 && operatorsArray.length!=1){
+        if(operatorsArray[i]==='*'|| operatorsArray[i]==='/'){
+            operandsArray[i] = arithematic(operatorsArray[i],operandsArray[i],operandsArray[i+1]);
+            operatorsArray = operatorsArray.filter((_val,j)=>i!=j);
+            operandsArray = operandsArray.filter((_val,j)=>i+1!=j);
+            
+        }
+        
+    }
+  result=arithematic(operatorsArray[0],operandsArray[0],operandsArray[1]);
 
+}
+ function arithematic(operator,firstoperands,secondoperands){
+    firstoperands=Number(firstoperands);
+    secondoperands=Number(secondoperands);
+    
+        switch(operator){
+            case "+": 
+                 return firstoperands+secondoperands;
+            case "-":
+                return firstoperands-secondoperands;
+            case "*":
+                return firstoperands*secondoperands;
+            case "/":
+                return firstoperands/secondoperands;
+            
+        }
+
+ }
